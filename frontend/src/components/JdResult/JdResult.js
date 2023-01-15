ab1ffeebed35524d41df9c378d9a5be62a77c494
divyanshu887 <singhdivyanshu364@gmail.com>
2023-01-15 21:03:16 +0530

    added detail modal

--- frontend/src/components/JdResult/JdResult.js
+++ frontend/src/components/JdResult/JdResult.js
@@ -1,93 +1,235 @@
-import React from 'react';
-import { DataGrid } from "@material-ui/data-grid";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
-import { Button, Link } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import axios from 'axios';
+import Detailmodal from '../candidateDetail/candidateDetails';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Button, Link } from '@material-ui/core';
 
 function JdResult() {
+  let empId = 'niharika-jain-838204191';
+  const [apiResponse, setApiResponse] = useState(null);
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      empId: [empId],
+    }),
+  };
+  console.log(requestOptions.body);
 
-const useStyles = makeStyles((theme) =>
-createStyles({
-  root: {
-    fontSize:'2rem',
-    marginLeft:'4%',
-    marginRight:'4%',
-    justifyContent: 'center',
-  }
-})
-);
+  useEffect(() => {
+    fetch('http://localhost:5000/api/empDetails/', requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        setApiResponse(data[empId]);
+        console.log(apiResponse, data[empId]);
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
-const handleClick = (event, cellValues) => {
-console.log(cellValues.row);
-};
+    // async function fetchData() {
+    //   try {
+    //     let data = await fetch(
+    //       'http://localhost:5000/api/empDetails/',
+    //       requestOptions
+    //     ).then(response => response.json());
 
-const handleCellClick = (param, event) => {
-event.stopPropagation();
-};
+    //     data = data[empId];
+    //     setApiResponse(data);
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // }
 
-const handleRowClick = (param, event) => {
-event.stopPropagation();
-};
+    // fetchData();
+  }, [empId]);
 
-const columns = [
-  {
-    field: "firstName",
-    headerName: "First Name",
-    width: 250,
-    
-  },
-{
-  field: "lastName",
-  headerName: "Last Name",
-  width: 250,
-},
-{
-  field: "relevance",
-  headerName: "Relevance",
-  width: 210,
-},
-{ field: "email", headerName: "Email", width: "400" },
-{
-  field: "Action",
-  width: 210,
-  renderCell: (cellValues) => {
-    return (
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        onClick={(event) => {
-          handleClick(event, cellValues);
-        }}
-      >
-        View Detail
-      </Button>
-    );
-  }
-},
-{
-  field: "Source",
-  width: 230,
-  renderCell: (cellValues) => {
-    return <Link href={`#${cellValues.row.url}`}>Link</Link>;
-  }
-}
-];
+  const [modalOpen, setModalOpen] = useState(false);
 
-const rows = [
-{ id: 1, firstName:"Jon",lastName: "Snow", relevance:"5",email: "abc@gmail.com", url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 2, firstName:"Tyrinn",lastName: "Lannister",relevance:"5", email: "test1@gmail.com",url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 3, firstName:"Jon" ,lastName: "Snow", relevance:"5",email: "abc3@gmail.com", url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 4, firstName:"Tyrinn",lastName: "Lannister",relevance:"5", email: "test4@gmail.com",url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 5, firstName:"Jon",lastName: "Snow",relevance:"5", email: "abc5@gmail.com", url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 6, firstName:"Tyrinn",lastName: "Lannister",relevance:"5", email: "test6@gmail.com",url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 7, firstName:"Jon",lastName: "Snow", relevance:"5",email: "abc7@gmail.com", url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 8, firstName:"Tyrinn",lastName: "Lannister",relevance:"5", email: "test8@gmail.com",url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 9, firstName:"Jon" ,lastName: "Snow", relevance:"5",email: "abc9@gmail.com", url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 10, firstName:"Tyrinn",lastName: "Lannister",relevance:"5", email: "test10@gmail.com",url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 11, firstName:"Jon",lastName: "Snow", relevance:"5",relevance:"5",email: "abc11@gmail.com", url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
-{ id: 12, firstName:"Tyrinn",lastName: "Lannister",relevance:"5", email: "test12@gmail.com",url: "https://www.linkedin.com/in/divyanshu-singh-486772195/" },
+  const useStyles = makeStyles(theme =>
+    createStyles({
+      root: {
+        fontSize: '2rem',
+        marginLeft: '4%',
+        marginRight: '4%',
+        justifyContent: 'center',
+      },
+    })
+  );
 
-];
+  const handleClick = (event, cellValues) => {
+    console.log(cellValues.row);
+  };
+
+  const handleCellClick = (param, event) => {
+    event.stopPropagation();
+  };
+
+  const handleRowClick = (param, event) => {
+    event.stopPropagation();
+  };
+
+  const columns = [
+    {
+      field: 'firstName',
+      headerName: 'First Name',
+      width: 250,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last Name',
+      width: 250,
+    },
+    {
+      field: 'relevance',
+      headerName: 'Relevance',
+      width: 210,
+    },
+    { field: 'email', headerName: 'Email', width: '400' },
+    {
+      field: 'Action',
+      width: 210,
+      renderCell: cellValues => {
+        return (
+          <>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              onClick={() => {
+                setModalOpen(true);
+              }}
+            >
+              View Detail
+            </Button>
+          </>
+        );
+      },
+    },
+    {
+      field: 'Source',
+      width: 230,
+      renderCell: cellValues => {
+        return <Link href={`#${cellValues.row.url}`}>Link</Link>;
+      },
+    },
+  ];
+
+  const rows = [
+    {
+      id: 2,
+      firstName: 'Tyrinn',
+      lastName: 'Lannister',
+      relevance: '5',
+      email: 'test1@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 3,
+      firstName: 'Jon',
+      lastName: 'Snow',
+      relevance: '5',
+      email: 'abc3@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 4,
+      firstName: 'Tyrinn',
+      lastName: 'Lannister',
+      relevance: '1',
+      email: 'test4@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 5,
+      firstName: 'Jon',
+      lastName: 'Snow',
+      relevance: '5',
+      email: 'abc5@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 6,
+      firstName: 'Tyrinn',
+      lastName: 'Lannister',
+      relevance: '3',
+      email: 'test6@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 7,
+      firstName: 'Jon',
+      lastName: 'Snow',
+      relevance: '5',
+      email: 'abc7@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 8,
+      firstName: 'Tyrinn',
+      lastName: 'Lannister',
+      relevance: '5',
+      email: 'test8@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 9,
+      firstName: 'Jon',
+      lastName: 'Snow',
+      relevance: '5',
+      email: 'abc9@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 10,
+      firstName: 'Tyrinn',
+      lastName: 'Lannister',
+      relevance: '4',
+      email: 'test10@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 11,
+      firstName: 'Jon',
+      lastName: 'Snow',
+      relevance: '5',
+      relevance: '5',
+      email: 'abc11@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+    {
+      id: 12,
+      firstName: 'Tyrinn',
+      lastName: 'Lannister',
+      relevance: '5',
+      email: 'test12@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+  ];
+
+  // if (apiResponse) {
+  //   rows.push({
+  //     id: 1,
+  //     firstName: apiResponse.profileData.fullname,
+  //     lastName: 'Snow',
+  //     relevance: '5',
+  //     email: 'abc@gmail.com',
+  //     url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+  //   });
+  // }
+
+  const datarow = [
+    {
+      id: 1,
+      // firstName: apiResponse.profileData.fullname,
+      lastName: 'Snow',
+      relevance: '5',
+      email: 'abc@gmail.com',
+      url: 'https://www.linkedin.com/in/divyanshu-singh-486772195/',
+    },
+  ];
+
   // const [tableData, setTableData] = useState([
   //   { name: "Raj", email: "Raj@gmail.com", status: "A",  },
   //   { name: "Mohan", email: "mohan@gmail.com", status: "I",  },
@@ -168,7 +310,7 @@ const rows = [
   //         filtering: true, paging: true, pageSizeOptions: [2, 5, 10, 20, 25, 50, 100], pageSize: 5,
   //         paginationType: "stepped", showFirstLastPageButtons: false, paginationPosition: "both", exportButton: true,
   //         exportAllData: true, exportFileName: "TableData", addRowPosition: "first", actionsColumnIndex: -1, selection: true,
-  //         showSelectAllCheckbox: true, showTextRowsSelected: false, 
+  //         showSelectAllCheckbox: true, showTextRowsSelected: false,
   //         grouping: true, columnsButton: true,
   //         rowStyle: (data, index) => index % 2 === 0 ? { background: "#f5f5f5" } : null,
   //         headerStyle: { background: "#f44336",color:"#fff"}
@@ -180,11 +322,12 @@ const rows = [
   const classes = useStyles();
 
   return (
-    <div style={{ height: "70vh", width: "100%" }}>
+    <div style={{ height: '70vh', width: '100%' }}>
+      {modalOpen && <Detailmodal setOpenModal={setModalOpen} />}
       <DataGrid
         rowHeight={70}
         className={classes.root}
-        rows={rows}
+        rows={apiResponse ? datarow : rows}
         columns={columns}
         pageSize={7}
         checkboxSelection
@@ -195,4 +338,4 @@ const rows = [
   );
 }
 
-export default JdResult
+export default JdResult;
---
+++ frontend/src/components/candidateDetail/candidateDetails.js
@@ -0,0 +1,267 @@
+// import React from 'react'
+import "./candidateDetails.css";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+// import "./AddQuesModal.css";
+// import FloatingLabel from 'react-bootstrap';
+import Form from "react-bootstrap/Form";
+// import FormCheckBox from "./CreateQuiz/FormCheckBox";
+// import { useAuth } from "../contexts/AuthContext";
+
+function Detailmodal({ setOpenModal,empData }) {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="titleCloseBtn">
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+        <div className="title">
+          <h1>Are You Sure You Want to Continue?</h1>
+        </div>
+        <div className="body">
+          {/* <h3>Name : {empData.name}</h3>
+          <h3> : {empData.name}</h3> */}
+        </div>
+        <div className="footer">
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+            id="cancelBtn"
+          >
+            Cancel
+          </button>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Detailmodal
+
+
+
+// function candidateDetails({ setOpenModal}) {
+// //   const { currentUser } = useAuth();
+
+//   const [tmp, setTmp] = useState({
+//     question: "",
+//     opt1: "",
+//     opt2: "",
+//     opt3: "",
+//     opt4: "",
+//     mmarks: 4,
+//   });
+
+//   const [correct, setCorrect] = useState({
+//     opt1: false,
+//     opt2: false,
+//     opt3: false,
+//     opt4: false,
+//     totalCorrect: 0,
+//     mmarks: 4,
+//   });
+
+//   // handle change event
+//   const handleChange = (e) => {
+//     e.preventDefault(); // prevent the default action
+//     setTmp({
+//       ...tmp,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+
+// //   const saveToDB = () => {
+// //     console.log(tmp);
+// //     const requestOptions = {
+// //       method: "POST",
+// //       headers: { "Content-Type": "application/json" },
+// //       body: JSON.stringify({
+// //         question: tmp,
+// //         answer: correct,
+// //         currentUser: currentUser.email.replace(".", ""),
+// //         quizID: new URLSearchParams(window.location.search).get("quizID"),
+// //       }),
+// //     };
+// //     fetch("http://localhost:5000/api/saveQues", requestOptions)
+// //       .then((response) => response.json())
+// //       .then((data) =>
+// //         setQuestion({
+// //           ...quesList,
+// //           [data.quesID]: tmp,
+// //         })
+// //       )
+// //       .catch((error) => {
+// //         console.log(error);
+// //       });
+// //   };
+
+//   return (
+//     <div className="modalBackground">
+//       <div className="modalContainer">
+//         <div className="titleCloseBtn">
+//           <button
+//             onClick={() => {
+//               setOpenModal(false);
+//             }}
+//           >
+//             <p style={{ fontSize: "15px", fontWeight: "bold" }}>X</p>
+//           </button>
+//         </div>
+//         <div className="body ">
+//           <Form>
+//             <Form.Group
+//               className="mb-3 ml-2"
+//               controlId="exampleForm.ControlTextarea1"
+//             >
+//               <Form.Label className="ml-1">Question:</Form.Label>
+//               <Form.Control
+//                 as="textarea"
+//                 rows={2}
+//                 name="question"
+//                 onChange={handleChange}
+//                 value={tmp.question}
+//               />
+//             </Form.Group>
+//             <Form.Group
+//               className="mb-3 ml-2"
+//               controlId="exampleForm.ControlTextarea1"
+//             >
+//               <Form.Label className="ml-1">Option 1:</Form.Label>
+//               <Form.Control
+//                 placeholder=""
+//                 name="opt1"
+//                 onChange={handleChange}
+//                 value={tmp.opt1}
+//               />
+//             </Form.Group>
+//             <Form.Group
+//               className="mb-3 ml-2"
+//               controlId="exampleForm.ControlTextarea1"
+//             >
+//               <Form.Label className="ml-1">Option 2:</Form.Label>
+//               <Form.Control
+//                 placeholder=""
+//                 name="opt2"
+//                 onChange={handleChange}
+//                 value={tmp.opt2}
+//               />
+//             </Form.Group>
+//             <Form.Group
+//               className="mb-3 ml-2"
+//               controlId="exampleForm.ControlTextarea1"
+//             >
+//               <Form.Label className="ml-1">Option 3:</Form.Label>
+//               <Form.Control
+//                 placeholder=""
+//                 name="opt3"
+//                 onChange={handleChange}
+//                 value={tmp.opt3}
+//               />
+//             </Form.Group>
+//             <Form.Group
+//               className="mb-3 ml-2"
+//               controlId="exampleForm.ControlTextarea1"
+//             >
+//               <Form.Label className="ml-1">Option 4:</Form.Label>
+//               <Form.Control
+//                 placeholder=""
+//                 name="opt4"
+//                 onChange={handleChange}
+//                 value={tmp.opt4}
+//               />
+//             </Form.Group>
+//             <Form.Group controlId="formFile" className="mb-3  row">
+//               <Form.Label className="col-3 ml-3 mt-2 ">
+//                 Correct Options :
+//               </Form.Label>
+//               <FormCheckBox
+//                 name="opt1"
+//                 optlabel="1"
+//                 correct={correct}
+//                 setCorrect={setCorrect}
+//               />
+//               &nbsp;
+//               <FormCheckBox
+//                 name="opt2"
+//                 optlabel="2"
+//                 correct={correct}
+//                 setCorrect={setCorrect}
+//               />
+//               &nbsp;
+//               <FormCheckBox
+//                 name="opt3"
+//                 optlabel="3"
+//                 correct={correct}
+//                 setCorrect={setCorrect}
+//               />
+//               &nbsp;
+//               <FormCheckBox
+//                 name="opt4"
+//                 optlabel="4"
+//                 correct={correct}
+//                 setCorrect={setCorrect}
+//               />
+//               <Form.Label className="col-3 ml-3 mt-2">
+//                 Maximum Marks :
+//               </Form.Label>
+//               <Form.Control
+//                 placeholder={0}
+//                 type="number"
+//                 aria-label="Username"
+//                 aria-describedby="basic-addon1"
+//                 style={{ maxWidth: "60px" }}
+//                 name="mmarks"
+//                 onChange={(e) => [
+//                   handleChange(e),
+//                   setCorrect({
+//                     ...correct,
+//                     mmarks: e.target.value,
+//                   }),
+//                 ]}
+//                 value={tmp.mmarks}
+//               />
+//             </Form.Group>
+
+//             <Form.Group controlId="formFile" className="mb-3  row">
+//               <Form.Label className="col-3 ml-3">Add Media :</Form.Label>
+//               <Form.Control type="file" className="media col-5 m-0 p-0" />
+//             </Form.Group>
+//           </Form>
+//         </div>
+//         <div className="footer block-example border-top border-dark">
+//           <Button
+//             size="sm"
+//             variant="primary "
+//             className="m-2"
+//             onClick={() => {
+//               setOpenModal(false);
+//             //   saveToDB();
+//             }}
+//           >
+//             Add
+//           </Button>
+//           <Button
+//             size="sm"
+//             variant="danger"
+//             className="m-2"
+//             onClick={() => {
+//               setOpenModal(false);
+//             }}
+//             id="cancelBtn"
+//           >
+//             Discard
+//           </Button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default candidateDetails
+// // export default AddQuesModal;
